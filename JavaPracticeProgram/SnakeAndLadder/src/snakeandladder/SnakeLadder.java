9abb5a34f60704200217a61d842ff8458fe86c84
priyanka897-star <priyankamore8997@gmail.com>
2021-09-27 22:14:37 +0530

    Added:Option for player to choose the next move

--- JavaPracticeProgram/SnakeAndLadder/src/snakeandladder/SnakeLadder.java
+++ JavaPracticeProgram/SnakeAndLadder/src/snakeandladder/SnakeLadder.java
@@ -3,40 +3,33 @@ package snakeandladder;
 public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("Starting of Snake and Ladder Game");
-	    int playerPresentStep = 0;
-		System.out.println("Starting the Game and You Are On step:" +playerPresentStep);
-		//int noPlay = 0;
+	    final int NoPlay = 0;
 		final int LADDER = 1;
 		final int SNAKE = 2;
-		while(playerPresentStep <= 100)
-		{
-		int dice = (int) Math.floor(Math.random() * 6 + 1);
-		System.out.println("The number on dice is " + dice);
-		int option = (int) Math.floor(Math.random() * 3);	
-		System.out.println("If you get option as 0 it is noPlay,1 it is ladder,2 it is snake " );
-		System.out.println("Player got option:" + option);
-		if(option == LADDER)
-		{
-			playerPresentStep += dice;
-			
-			System.out.println("Player are on step:" + playerPresentStep );
-			if(playerPresentStep == 100) {
-			   System.out.println("player Win the Game");
-			}
-		}
-			else if(option == SNAKE)
-			{
-				playerPresentStep -= dice;
-				if(playerPresentStep< 0)
-					playerPresentStep = 0;
-				System.out.println("Player are on step : " + playerPresentStep);
+		//variables
+		int dice = (int)((Math.random()*10) % 6);
+		int playerMove =(int)((Math.random()*10) %3 );
+		int playerCurrentPosition = 10;
+		 switch(playerMove)
+		    {
+		    case 0: 
+		    	System.out.println("No Movement");
+		    	break;
+		    case 1: 
+		    	playerCurrentPosition = LADDER;
+		    	playerCurrentPosition +=dice;
+		        System.out.println("Ladder move forward " + playerCurrentPosition);
+		        break;
+		    case 2:
+		    	playerCurrentPosition =SNAKE ;
+		    	playerCurrentPosition -=dice;
+		    System.out.println("Snakes move backward "+ playerCurrentPosition);
+		    break;
+		    default :
+		    	playerCurrentPosition +=dice;
+		    System.out.println("Movement to " +playerCurrentPosition );
+		    }
 				
-			}
-			else 
-			{
-				System.out.println("Player are on step :" + playerPresentStep);
-			}
-		}
 	}
 		
 	}	
