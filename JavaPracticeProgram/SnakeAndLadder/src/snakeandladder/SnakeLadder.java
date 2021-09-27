b1b2104c8b9476ed126bb5ef2bc3df74d43e1014
priyanka897-star <priyankamore8997@gmail.com>
2021-09-28 00:14:18 +0530

    Added:Code to playing two players

--- JavaPracticeProgram/SnakeAndLadder/src/snakeandladder/SnakeLadder.java
+++ JavaPracticeProgram/SnakeAndLadder/src/snakeandladder/SnakeLadder.java
@@ -2,35 +2,57 @@ package snakeandladder;
 
 public class SnakeLadder {
 	public static void main(String[] args) {
-            final int LADDER = 1;
-			final int SNAKE = 2;
-			 int dieRoll = 0;
-			int playerCurrentPosition = 0;
-			while( playerCurrentPosition < 100) 
-			{
-			   dieRoll++;
-			  int dice = (int)((Math.random()*10) % 6)+1;
-			  System.out.println("random number is:" +dice);   
-		      int position  = (int)((Math.random()*10) % 3);
-		      if(position == LADDER)
-		     {	
-		    	  playerCurrentPosition +=dice;
-		    if(playerCurrentPosition > 100)
-		    	playerCurrentPosition -=dice;
-		       System.out.println("Ladder move forward "+ playerCurrentPosition);
-		    }
-		    else if(position == SNAKE)
+		
+		int player1CurrentPosition= 0;
+		int player2CurrentPosition= 0;
+		int LADDER = 1;
+		int SNAKE = 2;
+		
+		while(player1CurrentPosition < 100 && player2CurrentPosition < 100) 
+		{
+		
+			int dice1 = (int)((Math.random()*10) % 6)+1;
+			System.out.println("Player1 random number is:" +dice1);
+			int dice2 = (int)((Math.random()*10) % 6)+1;
+			System.out.println(" Player2 random number is:" +dice2); 
+		
+		    int position  = (int)((Math.random()*10) % 3);
+	      if(position == LADDER)
+	      {	
+	    	player1CurrentPosition += dice1;
+	    	player2CurrentPosition += dice2; 
+		    if(player1CurrentPosition > 100 || player2CurrentPosition > 100 ) 
 		    {	
-		    	playerCurrentPosition -=dice;
-		    if(playerCurrentPosition < 0)
-		    	playerCurrentPosition= 0;
-		        System.out.println("Snakes move backward "+ playerCurrentPosition);
+		    	player1CurrentPosition -=dice1;
+		    	player2CurrentPosition -=dice2;
 		    }
-		    else
-		    {
-		    System.out.println("Player are on "+ playerCurrentPosition);
-		    }
-		    System.out.println("Number of time die to win the game :" + dieRoll);
-		}
+		    System.out.println("Ladder move forward 1 "+ player1CurrentPosition);
+		    System.out.println("Ladder move forward 2 "+ player2CurrentPosition);
+		    
+	    }
+	    else if(position == SNAKE)
+	    {	
+	    	player1CurrentPosition -= dice1;
+	    	player2CurrentPosition -= dice2;
+	    if(player1CurrentPosition < 0 || player2CurrentPosition < 0 ) 
+	    {
+	    	player1CurrentPosition = 0;
+	    	player2CurrentPosition = 0;
+	    }
+	    	System.out.println("Snakes move backward 1 "+ player1CurrentPosition);
+	    	System.out.println("Snakes move backward 2 "+ player2CurrentPosition);
+	    }
+	    else
+	    {
+	    	System.out.println("Player are on "+ player1CurrentPosition);
+	    	System.out.println("Player are on "+ player2CurrentPosition);
+	    }
+	    //System.out.println("Number of time die to win the game" + dieRoll);
+	    if(player1CurrentPosition == 100)
+	    	System.out.println("Player 1 Win");	
+	    else  if(player2CurrentPosition == 100)
+	    	System.out.println("Player 2 Win");	
+	    
+	  }
+	 }
 	}
-}
