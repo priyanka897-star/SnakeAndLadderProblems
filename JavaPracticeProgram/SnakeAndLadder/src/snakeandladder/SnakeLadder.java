38ff6e0fed766f0d22019961a896bbe1f7d16ff6
priyanka897-star <priyankamore8997@gmail.com>
2021-09-27 17:19:10 +0530

    Added:Functionality to start Game with player assign stating Position

---
+++ JavaPracticeProgram/SnakeAndLadder/src/snakeandladder/SnakeLadder.java
@@ -0,0 +1,8 @@
+package snakeandladder;
+
+public class SnakeLadder {
+	public static void main(String[] args) {
+		int startPosition = 0; 
+	    System.out.println("Game start and Player is at Position " +startPosition);
+ }
+}
