9506f5cea30691496e8dc6be0291d45de9db19f4
priyanka897-star <priyankamore8997@gmail.com>
2021-09-27 17:31:17 +0530

    Added:Code to Generate Random Die Number

--- JavaPracticeProgram/SnakeAndLadder/src/snakeandladder/SnakeLadder.java
+++ JavaPracticeProgram/SnakeAndLadder/src/snakeandladder/SnakeLadder.java
@@ -2,7 +2,9 @@ package snakeandladder;
 
 public class SnakeLadder {
 	public static void main(String[] args) {
-		int startPosition = 0; 
+		int startPosition = 0;
 	    System.out.println("Game start and Player is at Position " +startPosition);
- }
+		int dice = (int) Math.floor(Math.random() * 6 + 1);
+		System.out.println("The Number on dice is " + dice);
+	}
 }
