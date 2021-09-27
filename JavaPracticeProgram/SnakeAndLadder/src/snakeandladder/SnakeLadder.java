e9bd72d357f9b6452870c0ff06d5a7fcbc5d5fe3
priyanka897-star <priyankamore8997@gmail.com>
2021-09-27 19:21:24 +0530

    Added:Code to Repeat till the Player reaches the winning position

--- JavaPracticeProgram/SnakeAndLadder/src/snakeandladder/SnakeLadder.java
+++ JavaPracticeProgram/SnakeAndLadder/src/snakeandladder/SnakeLadder.java
@@ -2,32 +2,41 @@ package snakeandladder;
 
 public class SnakeLadder {
 	public static void main(String[] args) {
-		int startPosition = 0;
-		System.out.println("Game start and Player is at Position " +startPosition);
-		int dice = (int) Math.floor(Math.random() * 6 + 1);
-		System.out.println("The Number on dice is " + dice);
-		System.out.println("Welcome to Snake and Ladder Game");
-		int option = (int) Math.floor(Math.random() * 3);
-	    final int LADDER = 1;
+		System.out.println("Starting of Snake and Ladder Game");
+	    int playerPresentStep = 0;
+		System.out.println("Starting the Game and You Are On step:" +playerPresentStep);
+		//int noPlay = 0;
+		final int LADDER = 1;
 		final int SNAKE = 2;
-		int playerPresentStep = 10;
-		System.out.println("Present You are on Step:" + playerPresentStep);
+		while(playerPresentStep <= 100)
+		{
+		int dice = (int) Math.floor(Math.random() * 6 + 1);
+		System.out.println("The number on dice is " + dice);
+		int option = (int) Math.floor(Math.random() * 3);	
 		System.out.println("If you get option as 0 it is noPlay,1 it is ladder,2 it is snake " );
+		System.out.println("Player got option:" + option);
 		if(option == LADDER)
 		{
 			playerPresentStep += dice;
-			System.out.println("Player Moves on :" + playerPresentStep);
-		}
-		else if(option == SNAKE)
-		{
-				playerPresentStep-= dice;
-				System.out.println("Player Moves on  : " + playerPresentStep);
-		}
-		else 
-			{
-				System.out.println("Player Moves on :" + playerPresentStep);
+			
+			System.out.println("Player are on step:" + playerPresentStep );
+			if(playerPresentStep == 100) {
+			   System.out.println("player Win the Game");
 			}
-		System.out.println("player got option:" + option);
+		}
+			else if(option == SNAKE)
+			{
+				playerPresentStep -= dice;
+				if(playerPresentStep< 0)
+					playerPresentStep = 0;
+				System.out.println("Player are on step : " + playerPresentStep);
+				
+			}
+			else 
+			{
+				System.out.println("Player are on step :" + playerPresentStep);
+			}
+		}
 	}
-
-}
+		
+	}	
